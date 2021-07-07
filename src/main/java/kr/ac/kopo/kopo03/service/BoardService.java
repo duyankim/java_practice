068189192c16421837bb696456664f2155a90ce9
duyankim <arbolkim@gmail.com>
2021-07-07 20:47:25 +0900

    [feat] `BoardService` interface, `BoardServiceImpl` using JpaRepository method

---
+++ src/main/java/kr/ac/kopo/kopo03/service/BoardService.java
@@ -0,0 +1,18 @@
+package kr.ac.kopo.kopo03.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import kr.ac.kopo.kopo03.domain.Board;
+
+@Service
+public interface BoardService {
+    void create(Board board);
+    void update(Board board);
+    void delete(Board board);
+    Optional<Board> viewOne(int id);
+    List<Board> viewAll();
+    String[] viewAllTitles();
+}
---
+++ src/main/java/kr/ac/kopo/kopo03/service/BoardServiceImpl.java
@@ -0,0 +1,60 @@
+package kr.ac.kopo.kopo03.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.ac.kopo.kopo03.domain.Board;
+import kr.ac.kopo.kopo03.repository.BoardRepository;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+	
+	private final BoardRepository boardRepository;
+	
+	@Autowired
+	public BoardServiceImpl(BoardRepository boardRepository) {
+		this.boardRepository = boardRepository;
+	}
+
+	@Override
+	public void create(Board board) {
+		boardRepository.save(board);
+	}
+
+	@Override
+	public void update(Board board) {
+		boardRepository.save(board);
+	}
+
+	@Override
+	public void delete(Board board) {
+		boardRepository.delete(board);
+	}
+
+	@Override
+	public Optional<Board> viewOne(int id) {
+		Optional<Board> b = boardRepository.findById(id);
+		return b;
+	}
+
+	@Override
+	public List<Board> viewAll() {
+		return boardRepository.findAll();
+	}
+
+	@Override
+	public String[] viewAllTitles() {
+		List<Board> boards = boardRepository.findAll();
+		String[] titles = new String[boards.size()];
+		int i = 0;
+		for (Board b : boards) {
+			titles[i] = b.getTitle();
+			i++;
+		}
+		return titles;
+	}
+
+}

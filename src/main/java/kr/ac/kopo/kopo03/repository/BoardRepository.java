b0dd38de07a6b2dc1dfde7a8f215b68ecb752232
duyankim <arbolkim@gmail.com>
2021-07-07 20:46:03 +0900

    [feat] `BoardRepository` interface extends JpaRepository

---
+++ src/main/java/kr/ac/kopo/kopo03/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package kr.ac.kopo.kopo03.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import kr.ac.kopo.kopo03.domain.Board;
+
+public interface BoardRepository extends JpaRepository<Board, Integer>, JpaSpecificationExecutor<Board>{
+	
+}

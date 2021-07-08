9b6793de2ba0ba81b8106535bbc5e872c15fee17
duyankim <arbolkim@gmail.com>
2021-07-08 20:55:45 +0900

    [feat] `BoardItemRepository` interface implements JpaRepository

---
+++ src/main/java/kr/ac/kopo/kopo03/repository/BoardItemRepository.java
@@ -0,0 +1,17 @@
+package kr.ac.kopo.kopo03.repository;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import kr.ac.kopo.kopo03.domain.Board;
+import kr.ac.kopo.kopo03.domain.BoardItem;
+
+@Repository
+public interface BoardItemRepository  extends JpaRepository<BoardItem, Integer>, JpaSpecificationExecutor<BoardItem>{
+	BoardItem findFirstByOrderByIdDesc();
+	Page<BoardItem> findByBoard(int board_id, Pageable pageable);
+}
+

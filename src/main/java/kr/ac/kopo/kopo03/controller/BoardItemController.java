236cf6653194a030778c4d233827ae91f8dbaa5f
duyankim <arbolkim@gmail.com>
2021-07-09 18:28:28 +0900

    [fix] `BoardItemController` RequestParam passes boardId

--- src/main/java/kr/ac/kopo/kopo03/controller/BoardItemController.java
+++ src/main/java/kr/ac/kopo/kopo03/controller/BoardItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.ac.kopo.kopo03.domain.BoardItem;
 import kr.ac.kopo.kopo03.service.BoardItemService;
@@ -17,14 +18,14 @@ public class BoardItemController {
 	private BoardItemService boardItemService;
 	
 	@GetMapping("/postlist")
-	public String postlist(Model model) {
+	public String postlist(Model model, @RequestParam("boardid") int boardId) {
 		List<BoardItem> shortenContentList = boardItemService.viewAll();
 		for (BoardItem item : shortenContentList) {
 			int end;
 			String preview;
 			
 			if (item.getContent() != null) {
-				end =  item.getContent().length() > 50 ? 50 : item.getContent().length();
+				end =  item.getContent().length() > 80 ? 80 : item.getContent().length();
         		preview = item.getContent().substring(0, end) + "...";
         	} else {
         		preview = "";
@@ -32,6 +33,14 @@ public class BoardItemController {
 			item.setContent(preview);
 		}
 	    model.addAttribute("postlist", shortenContentList);
+	    model.addAttribute("board", boardId);
 	    return "postlist";
+	}
+	
+	@GetMapping("/postview")
+	public String postview(Model model, @RequestParam("postid") int postId) {
+	    BoardItem post = boardItemService.viewOne(postId).get();
+	    model.addAttribute("post", post);
+	    return "postview";
 	}
 }

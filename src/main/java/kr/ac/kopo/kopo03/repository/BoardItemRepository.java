4939f0d35f405ad2d585143a3cc1363c114db844
duyankim <arbolkim@gmail.com>
2021-07-11 23:47:17 +0900

    [fix] id type long to int

--- src/main/java/kr/ac/kopo/kopo03/repository/BoardItemRepository.java
+++ src/main/java/kr/ac/kopo/kopo03/repository/BoardItemRepository.java
@@ -1,12 +1,11 @@
 package kr.ac.kopo.kopo03.repository;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import kr.ac.kopo.kopo03.domain.Board;
 import kr.ac.kopo.kopo03.domain.BoardItem;
 
 @Repository
@@ -14,4 +13,3 @@ public interface BoardItemRepository  extends JpaRepository<BoardItem, Integer>,
 	BoardItem findFirstByOrderByIdDesc();
 	Page<BoardItem> findByBoard(int board_id, Pageable pageable);
 }
-
--- src/main/java/kr/ac/kopo/kopo03/repository/BoardRepository.java
+++ src/main/java/kr/ac/kopo/kopo03/repository/BoardRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import kr.ac.kopo.kopo03.domain.Board;
 
-public interface BoardRepository extends JpaRepository<Board, Long>, JpaSpecificationExecutor<Board>{
+public interface BoardRepository extends JpaRepository<Board, Integer>, JpaSpecificationExecutor<Board>{
 
 }
